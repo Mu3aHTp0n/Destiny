5ded38ba121f2286cbdeb8896213908c235784d0
Mu3aHTp0n <kovalevartur05@gmail.com>
2023-05-29 00:00:00 +0300

    Добавьте файлы проекта.

---
+++ ViewModel/IntConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Destiny.ViewModel
+{
+    internal class IntConverter
+    {
+        public object Convert(object value) => (int)value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
